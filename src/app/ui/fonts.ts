0436b137038011ff6c998e7c8c931a9b19619adb
rebelcl0ud <y0j0v33@gmail.com>
2024-04-05 23:20:29 -0500

    add: fonts

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Nunito } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const nunito = Nunito({
+    weight: ['200', '300', '400', '700'],
+    subsets: ['latin']
+})
