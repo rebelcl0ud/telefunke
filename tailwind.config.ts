5876bd08a0245b991c0c36e47594d41c73c772a3
rebelcl0ud <y0j0v33@gmail.com>
2024-04-05 23:06:41 -0500

    update: remove incl globals, add tailwind config w colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                cameo: "#f1ddbf", // bg
+                blueZodiac: "#3b4455;", // text
+                yucca: "#78938a", // deck 1
+                darkSeaGreen: "#92ba92", // deck 2
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
