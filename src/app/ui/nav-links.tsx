0d94aced14bde60980836ccc1565b12a009b06cf
rebelcl0ud <y0j0v33@gmail.com>
2024-04-05 23:21:54 -0500

    add: sidenav + nav-links

---
+++ src/app/ui/nav-links.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { HomeIcon, UserIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    { name: "home", href: "/", icon: HomeIcon },
+    { name: "profile", href: "/profile", icon: UserIcon },
+];
+
+export default function NavLinks() {
+    const pathname = usePathname();
+    return (
+        <>
+            {links.map((link) => {
+                const LinkIcon = link.icon;
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className={clsx(
+                            "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                            {
+                                "bg-sky-100 text-blue-600":
+                                    pathname === link.href,
+                            },
+                        )}
+                    >
+                        <LinkIcon className="w-6" />
+                        <p
+                            className={clsx("hidden md:block", {
+                                "pointer-events-none opacity-50 cursor-not-allowed":
+                                    link.href === "/profile",
+                            })}
+                        >
+                            {link.name}
+                        </p>
+                    </Link>
+                );
+            })}
+        </>
+    );
+}
